2dc13012cc847a8abcf5c4aff30d6b0ce7f39e9f
Gregor Noczinski <gregor@noczinski.eu>
2023-01-31 10:01:23 +0100

    * Updatest to latest versions * Adjusted patterns

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,28 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
       }
-    ]
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
   },
   devServer: {
-    contentBase: [
-      path.join(__dirname, "dist"),
-      path.join(__dirname, "data"),
-      path.join(__dirname, "static")
+    static: [
+      path.join(__dirname, 'data'),
+      path.join(__dirname, 'static')
     ],
     compress: true,
     port: 9000
